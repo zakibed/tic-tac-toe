dd9edda5fa200cebe1310179fa7aa8a4fb883085
zakzinzan123 <zakzinzan123@gmail.com>
2022-12-30 19:46:49 +0800

    Change appearance and add two player functionality

--- script.js
+++ script.js
@@ -1,66 +1,74 @@
 const gameBoardGrid = document.querySelector('#gameboard');
-const boardCell = document.querySelectorAll('#gameboard > div');
+const boardCell = document.querySelectorAll('.cell');
 
-const Player = (name) => {
+const Player = (name, marker) => {
     let health = 3;
 
     const lose = () => console.log('YOU LOSE!');
     const removeHealth = () => (health > 0 ? health-- : lose());
 
-    return { name, health, removeHealth };
+    return { name, marker, health, removeHealth };
 };
 
 const gameBoard = (() => {
-    const arr = [];
-    const playerOne = Player('playerOne');
-    const playerTwo = Player('playerTwo');
+    const board = [];
+    const playerOne = Player('playerOne', 'x');
+    const playerTwo = Player('playerTwo', 'o');
+    let currentPlayer = playerOne;
+
+    const addMarker = (e) => {
+        const index = e.target.dataset.index;
+
+        if (!board[index]) {
+            board[index] = currentPlayer.marker;
+
+            displayController.showBoard();
+            _checkWinner(currentPlayer);
+
+            currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
+        }
+    };
 
     const _getThreeInARow = (i, end, increment = 1) => {
         const row = [];
 
         while (i <= end) {
-            row.push(arr[i]);
+            row.push(board[i]);
             i += increment;
         }
 
         return row;
     };
 
-    const _checkWinner = () => {
+    const _checkWinner = (player) => {
+        const showWinner = () => console.log(`%c${player.name} WINS!`, 'color: lime;');
+
         // check columns
         for (let i = 0; i < 3; i++) {
-            const jackpot = _getThreeInARow(i, i + 6, 3).every((n) => n === 'x');
-            if (jackpot) console.log('YAHOO!');
+            const jackpot = _getThreeInARow(i, i + 6, 3).every((n) => n === player.marker);
+            if (jackpot) showWinner();
         }
 
         // check rows
         for (let i = 0; i <= 6; i += 3) {
-            const jackpot = _getThreeInARow(i, i + 2).every((n) => n === 'x');
-            if (jackpot) console.log('YAHOO!');
+            const jackpot = _getThreeInARow(i, i + 2).every((n) => n === player.marker);
+            if (jackpot) showWinner();
         }
 
         // check diagonally
         for (let i = 0; i < 3; i += 2) {
             const jackpot =
                 i === 0
-                    ? _getThreeInARow(i, 8, 4).every((n) => n === 'x')
-                    : _getThreeInARow(i, 6, 2).every((n) => n === 'x');
-            if (jackpot) console.log('YAHOO!');
-        }
-    };
-
-    const addMarker = (e) => {
-        const index = e.target.dataset.index;
-
-        if (!arr[index]) {
-            arr[index] = 'x';
-            displayController.showBoard();
-            _checkWinner();
+                    ? _getThreeInARow(i, 8, 4).every((n) => n === player.marker)
+                    : _getThreeInARow(i, 6, 2).every((n) => n === player.marker);
+            if (jackpot) showWinner();
         }
     };
 
     return {
-        arr,
+        board,
+        playerOne,
+        playerTwo,
         addMarker
     };
 })();
@@ -70,7 +78,7 @@ const displayController = (() => {
         for (let i = 0; i < 9; i++) {
             const cell = document.querySelector(`[data-index='${i}']`);
 
-            cell.textContent = gameBoard.arr[i];
+            cell.textContent = gameBoard.board[i];
         }
     };
 
