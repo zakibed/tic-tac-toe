70ef10d1b874c0d296b58bc2d3e9aa1b2809e6a4
zakzinzan123 <zakzinzan123@gmail.com>
2023-01-09 21:59:49 +0800

    Add play again btn, game over indicator, game restart, and refactor code

--- script.js
+++ script.js
@@ -5,14 +5,17 @@ const Player = (name, marker) => {
     const winRound = (enemy) => {
         enemy.health--;
 
-        display.healthBar(enemy);
+        display.removeHealthBar(enemy);
 
-        if (enemy.health > 0) {
-            Gameboard.reset();
-            display.roundResult(name, 'WINS this round!');
-        } else {
+        if (enemy.health === 0) {
             display.roundResult(name, 'WINS THE GAME!', 'var(--green)');
             Gameboard.toggle(false);
+            game.isOver = true;
+
+            setTimeout(display.endGame, 2000);
+        } else {
+            display.roundResult(name, 'WINS this round!');
+            Gameboard.reset();
         }
     };
 
@@ -29,7 +32,7 @@ const Gameboard = (() => {
         display.roundResult('', 'A new round begins!');
 
         setTimeout(() => {
-            toggle(true);
+            if (!(game.playerOne.isBot && game.playerTwo.isBot)) toggle(true);
             display.roundResult('', '');
         }, 1000);
     };
@@ -58,6 +61,7 @@ const Gameboard = (() => {
             board[index] = game.currentPlayer.marker;
 
             display.board();
+
             game.checkWinner(game.currentPlayer);
 
             game.currentPlayer =
@@ -79,6 +83,7 @@ const game = (() => {
     let playerOne = Player('Player 1', 'x');
     let playerTwo = Player('Player 2', 'o');
     let currentPlayer = playerOne;
+    let isOver = false;
 
     const _tieRound = () => {
         Gameboard.reset();
@@ -113,9 +118,11 @@ const game = (() => {
             return Gameboard.board[num] ? getRandomMove() : num;
         };
 
-        Gameboard.toggle(true);
+        if (!game.isOver) Gameboard.toggle(true);
 
-        if (Gameboard.board.filter((x) => x).length < 9) Gameboard.addMarker(null, getRandomMove());
+        if (!game.isOver && Gameboard.board.filter((x) => x).length < 9) {
+            Gameboard.addMarker(null, getRandomMove());
+        }
     };
 
     const start = (e) => {
@@ -123,13 +130,27 @@ const game = (() => {
         display.gameBoard.style.display = 'grid';
 
         display.playerType();
-        display.players(currentPlayer);
+        display.players(playerOne);
         display.form.reset();
         display.form.style.display = 'none';
 
-        currentPlayer.isBot ? setTimeout(playBotTurn, 1000) : Gameboard.toggle(true);
+        playerOne.isBot ? setTimeout(playBotTurn, 1000) : Gameboard.toggle(true);
 
         e.preventDefault();
+    };
+
+    const restart = () => {
+        Gameboard.board.length = 0;
+
+        display.board();
+        display.fullHealth();
+        display.players(playerOne);
+        display.playAgainBtn.style.display = 'none';
+
+        game.currentPlayer = playerOne;
+        game.isOver = false;
+
+        playerOne.isBot ? setTimeout(playBotTurn, 1000) : Gameboard.toggle(true);
     };
 
     const checkWinner = (player) => {
@@ -161,27 +182,50 @@ const game = (() => {
         }
     };
 
-    return { playerOne, playerTwo, currentPlayer, playBotTurn, start, checkWinner };
+    return {
+        playerOne,
+        playerTwo,
+        currentPlayer,
+        isOver,
+        playBotTurn,
+        start,
+        restart,
+        checkWinner
+    };
 })();
 
 const display = (() => {
     const _winner = document.querySelector('#results p:first-child');
     const _results = document.querySelector('#results p:last-child');
+
     const _playerOneHealth = document.querySelector('.player.one .health');
     const _playerTwoHealth = document.querySelector('.player.two .health');
+
     const _playerOneTurn = document.querySelector('.player.one .turn');
     const _playerTwoTurn = document.querySelector('.player.two .turn');
+
     const _playerOneType = document.querySelector('#player-one-player');
     const _playerTwoType = document.querySelector('#player-two-player');
+
     const startGameBtn = document.querySelector('#start-game-btn');
+    const playAgainBtn = document.querySelector('#play-again-btn');
+
     const gameBoard = document.querySelector('#gameboard');
     const boardCell = document.querySelectorAll('.cell');
+
     const form = document.querySelector('form');
 
     const board = () => {
         for (let i = 0; i < 9; i++) {
             document.querySelector(`[data-index='${i}']`).textContent = Gameboard.board[i];
         }
+    };
+
+    const endGame = () => {
+        roundResult('', '');
+
+        playAgainBtn.style.display = 'block';
+        playAgainBtn.addEventListener('click', game.restart);
     };
 
     const roundResult = (name, text, color = 'white') => {
@@ -196,27 +240,32 @@ const display = (() => {
         _results.style.color = color;
     };
 
-    const players = (player) => {
-        const showTurn = (playerTurn, enemyTurn) => {
-            playerTurn.closest('p').style.display = 'block';
-            enemyTurn.closest('p').style.display = 'none';
-            playerTurn.textContent = `${player.name}'s`;
+    const fullHealth = () => {
+        const replenish = (parent) => {
+            const health = parent.children.length;
+
+            for (let i = 0; i < 5 - health; i++) {
+                const div = document.createElement('div');
+                parent.appendChild(div);
+            }
         };
 
-        player === game.playerOne
-            ? showTurn(_playerOneTurn, _playerTwoTurn)
-            : showTurn(_playerTwoTurn, _playerOneTurn);
+        game.playerOne.health = 5;
+        game.playerTwo.health = 5;
+
+        replenish(_playerOneHealth);
+        replenish(_playerTwoHealth);
     };
 
-    const healthBar = (enemy) => {
-        const removeHealth = (parent) => {
+    const removeHealthBar = (enemy) => {
+        const remove = (parent) => {
             const health = parent.lastElementChild;
 
             health.style.background = 'red';
             setTimeout(() => parent.removeChild(health), 200);
         };
 
-        removeHealth(enemy === game.playerOne ? _playerOneHealth : _playerTwoHealth);
+        remove(enemy === game.playerOne ? _playerOneHealth : _playerTwoHealth);
     };
 
     const playerType = () => {
@@ -231,16 +280,31 @@ const display = (() => {
         }
     };
 
+    const players = (player) => {
+        const showTurn = (playerTurn, enemyTurn) => {
+            playerTurn.closest('p').style.display = 'block';
+            enemyTurn.closest('p').style.display = 'none';
+            playerTurn.textContent = `${player.name}'s`;
+        };
+
+        player === game.playerOne
+            ? showTurn(_playerOneTurn, _playerTwoTurn)
+            : showTurn(_playerTwoTurn, _playerOneTurn);
+    };
+
     return {
         startGameBtn,
+        playAgainBtn,
         gameBoard,
         boardCell,
-        board,
         form,
+        board,
+        endGame,
         roundResult,
+        fullHealth,
+        removeHealthBar,
         playerType,
-        players,
-        healthBar
+        players
     };
 })();
 
